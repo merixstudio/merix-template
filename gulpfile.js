a87358a51b5738af234867ff27714f924ad19c0e
Pawel Dzierzbicki <p.dzierzbicki@merixstudio.com>
2014-02-07 15:09:15 +0100

    Added swig based templates support using node.js and gulp.
    --HG--
    branch : future

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var swig = require('gulp-swig');
+
+swig({'defaults': {'cache': false}});
+
+var TEMPLATES_SRC = './*.html';
+var STYLES_SRC = './styles/**/*';
+var SCRIPTS_SRC = './scripts/**/*';
+var FONTS_SRC = './fonts/**/*';
+var IMAGES_SRC = './images/**/*';
+var MEDIA_SRC = './media/**/*';
+
+gulp.task('templates', function() {
+    return gulp.src(TEMPLATES_SRC).pipe(swig()).pipe(gulp.dest('./_build/'));
+});
+
+gulp.task('styles', function() {
+    return gulp.src(STYLES_SRC).pipe(gulp.dest('./_build/styles/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(SCRIPTS_SRC).pipe(gulp.dest('./_build/scripts/'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(FONTS_SRC).pipe(gulp.dest('./_build/fonts/'));
+});
+
+gulp.task('images', function() {
+    return gulp.src(IMAGES_SRC).pipe(gulp.dest('./_build/images/'));
+});
+
+gulp.task('media', function() {
+    return gulp.src(MEDIA_SRC).pipe(gulp.dest('./_build/media/'));
+});
+
+gulp.task('server', ['templates', 'styles', 'scripts', 'fonts', 'images', 'media'], function() {
+    gulp.watch(TEMPLATES_SRC, ['templates']);
+    gulp.watch(STYLES_SRC, ['styles']);
+    gulp.watch(SCRIPTS_SRC, ['scripts']);
+    gulp.watch(FONTS_SRC, ['fonts']);
+    gulp.watch(IMAGES_SRC, ['images']);
+    gulp.watch(MEDIA_SRC, ['media']);
+});
+
+gulp.task('default', ['server']);

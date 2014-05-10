15ff88b4eda6eb2fdf458cefb0329eccbad42a4c
Tomasz Grajewski <t.grajewski@merixstudio.com>
2014-05-10 20:46:21 +0200

    Fixed the callable decorator to work properly in IE8 when used by multiple objects.
    --HG--
    branch : future

--- scripts/nebula/utils/callable.js
+++ scripts/nebula/utils/callable.js
@@ -25,36 +25,41 @@
              */
             function sandbox(code) {
                 var iframe = document.createElement('iframe');
+                var script = document.createElement('script');
                 iframe.style.display = 'none';
                 document.documentElement.appendChild(iframe);
                 iframe = frames[frames.length - 1];
-                var script = document.createElement('script');
                 script.text = code;
                 iframe.document.appendChild(script);
+                return iframe;
             }
+
+
+            function callable(cls) {
+                var oldCallable = window.callable, _callable;
+                sandbox("\
+                    if (!Function.prototype.bind)\
+                        Function.prototype.bind = parent.Function.prototype.bind;\
+                    parent.callable = function(F) {\
+                        var C = function() {\
+                            var obj = function() {\
+                                return C.prototype.call.apply(obj, arguments);\
+                            };\
+                            F.apply(obj, arguments);\
+                            return obj;\
+                        };\
+                        C.prototype = Function.prototype;\
+                        return C;\
+                    };\
+                    parent.callable.prototype = Function.prototype;");
+                _callable = window.callable;
+                window.callable = oldCallable;
+                return _callable(cls);
+            }
+
 
             if (!Function.prototype.bind)
                 throw new Error("`Function.prototype.bind` polyfill is required for the 'callable' module to work in this browser");
-
-            var oldCallable = window.callable, callable;
-            sandbox("\
-                if (!Function.prototype.bind)\
-                    Function.prototype.bind = parent.Function.prototype.bind;\
-                parent.callable = function(F) {\
-                    var C = function() {\
-                        var obj = function() {\
-                            return C.prototype.call.apply(obj, arguments);\
-                        };\
-                        F.apply(obj, arguments);\
-                        return obj;\
-                    };\
-                    C.prototype = Function.prototype;\
-                    return C;\
-                };\
-                parent.callable.prototype = Function.prototype;\
-                ");
-            callable = window.callable;
-            window.callable = oldCallable;
             return callable;
         };
 

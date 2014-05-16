e17b02a549c5bee87ee23819d65ceac21a13dc8d
Kamil Brenk <k.brenk@merixstudio.com>
2014-05-16 09:56:40 +0200

    Added CSS prefixer for Gulp
    --HG--
    branch : future

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,18 @@ var gulp = require('gulp');
 var swig = require('gulp-swig');
 var map = require('map-stream');
 var html = require('htmltidy');
+var prefix = require('gulp-autoprefixer');
 var exec = require('child_process').exec;
 var sizeOf = require('image-size');
 var Table = require('cli-table');
 
 var TEMPLATES_SRC = './templates/*.html';
 var TEMPLATES_TO_BUILD = './templates/!(_)*.html';
+var STYLES_SRC = './styles/**/*';
+var STYLES_TO_BUILD = './_build/styles/';
 var BUILD_SRC = './_build/**/*';
 
 var COPY_TASKS = {
-    'styles': ['./styles/**/*', './_build/styles/'],
     'scripts': [['./scripts/**/*', '!./scripts/nebula/test.js', '!./scripts/nebula/tests/**/*'], './_build/scripts/'],
     'fonts': ['./fonts/**/*', './_build/fonts/'],
     'images': ['./images/**/*', './_build/images/'],
@@ -86,6 +88,10 @@ gulp.task('templates', function() {
     return gulp.src(TEMPLATES_TO_BUILD).pipe(swig()).pipe(tidy()).pipe(gulp.dest('./_build/'));
 });
 
+gulp.task('styles', function() {
+    return gulp.src(STYLES_SRC).pipe(prefix()).pipe(gulp.dest(STYLES_TO_BUILD));
+});
+
 
 for (var name in COPY_TASKS)
     copyFilesTask(name, COPY_TASKS[name][0], COPY_TASKS[name][1]);
@@ -122,7 +128,7 @@ gulp.task('memory_usage', ['memory_usage_count'], function() {
 
 gulp.task('server', ['templates', 'styles', 'scripts', 'fonts', 'images', 'media'], function() {
     gulp.watch(TEMPLATES_SRC, ['templates']);
-    gulp.watch(COPY_TASKS.styles[0], ['styles']);
+    gulp.watch(STYLES_SRC, ['styles']);
     gulp.watch(COPY_TASKS.scripts[0], ['scripts']);
     gulp.watch(COPY_TASKS.fonts[0], ['fonts']);
     gulp.watch(COPY_TASKS.images[0], ['images']);

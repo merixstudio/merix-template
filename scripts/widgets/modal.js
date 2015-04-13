159fb3dbe2db56cb6021d7246a09865137dd943b
Marcin Wasilewski <m.wasilewski@merixstudio.com>
2015-04-13 13:42:28 +0200

    Add getScrollBarWidth() to modal.js

--- scripts/widgets/modal.js
+++ scripts/widgets/modal.js
@@ -50,7 +50,33 @@ define('widgets/modal', ['jquery', 'nebula/signal'], function(jQuery, Signal) {
         modal.onModalOpen.send();
 
         jQuery('html').addClass('no-scroll');
-        jQuery('body').css('margin-right', '17px');
+        jQuery('body').css('margin-right', getScrollBarWidth());
+        
+        function getScrollBarWidth () {
+            var inner = document.createElement('p');
+            inner.style.width = "100%";
+            inner.style.height = "200px";
+
+            var outer = document.createElement('div');
+            outer.style.position = "absolute";
+            outer.style.top = "0px";
+            outer.style.left = "0px";
+            outer.style.visibility = "hidden";
+            outer.style.width = "200px";
+            outer.style.height = "150px";
+            outer.style.overflow = "hidden";
+            outer.appendChild(inner);
+
+            document.body.appendChild (outer);
+            var w1 = inner.offsetWidth;
+            outer.style.overflow = 'scroll';
+            var w2 = inner.offsetWidth;
+            if (w1 == w2) w2 = outer.clientWidth;
+
+            document.body.removeChild (outer);
+
+            return (w1 - w2);
+          };
     };
 
     modal.close = function() {

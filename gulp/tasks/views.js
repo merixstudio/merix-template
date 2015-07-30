ecef9a956d56eb4dbef6f20c320111fe90cba8bd
Mikołaj Stolarski <m.stolarski@merixstudio.com>
2015-07-30 13:39:19 +0200

    Cleanup
    --HG--
    branch : browserify

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -1,18 +1,18 @@
-var config       = require('../config');
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var nunjucks     = require('gulp-nunjucks');
+var config      = require('../config');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var swig        = require('gulp-swig');
 
 gulp.task('views:dev', function() {
     return gulp.src(config.views.src)
-        .pipe(nunjucks())
+        .pipe(swig({ cache: false }))
         .pipe(gulp.dest(config.views.dest))
         .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('views:production', function() {
     return gulp.src(config.views.src)
-        .pipe(nunjucks())
+        .pipe(swig({ cache: false }))
         .pipe(gulp.dest(config.views.dest));
 });
 

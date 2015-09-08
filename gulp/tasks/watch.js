4c841dd06b307c69b43d7b55ca0caaa4813ba8e7
Mateusz Anioła <none@none>
2015-09-08 11:14:43 +0200

    Removed unused styles. Added jshintrc. Updated gulp watch task to watch for media changes.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,6 +7,7 @@ gulp.task('watch', ['browserSync'], function(){
     gulp.watch(config.fonts.src, ['fonts']);
     gulp.watch(config.styles.src, ['styles:dev']);
     gulp.watch(config.images.src, ['images:dev', 'reload']);
+    gulp.watch(config.medias.src, ['media:dev', 'reload']);
     gulp.watch(config.views.watch, ['views:dev']);
 
 });

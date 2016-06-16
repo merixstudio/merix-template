f1d9342b40b796288ef7818b1f7536153de240e3
Adam Szymański <a.szymanski@merixstudio.com>
2016-06-16 13:37:48 +0200

    Fixed gulp task for rendering views, which reloads browser too quickly.

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -1,16 +1,21 @@
 var config          = require('../config');
 var gulp            = require('gulp');
+var runSequence     = require('run-sequence');
 var bs              = require('browser-sync');
 var nunjucksRender  = require('gulp-nunjucks-render');
 
-gulp.task('views:dev', function() {
-    var browserSync = bs.get('pigie');
-
+gulp.task('views:dev:render', function() {
     nunjucksRender.nunjucks.configure(['templates/'], {watch: true});
     return gulp.src(config.views.src)
         .pipe(nunjucksRender())
-        .pipe(gulp.dest(config.views.dest))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest(config.views.dest));
+});
+
+gulp.task('views:dev', function(cb) {
+    var browserSync = bs.get('pigie');
+
+    runSequence('views:dev:render', browserSync.reload);
+    cb();
 });
 
 gulp.task('views:production', function() {

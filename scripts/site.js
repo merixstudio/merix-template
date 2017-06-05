e38db401039166dfd7fd9c94105351a98891cdfc
Dominik Lochynski <d.lochynski@merixstudio.com>
2017-06-05 10:56:07 +0200

    Replace jshint with eslint, change js indent_size

--- scripts/site.js
+++ scripts/site.js
@@ -1,23 +1,30 @@
 import 'babel-polyfill';
 import jQuery from 'jquery';
+
+/* eslint-disable no-multi-assign */
 window.$ = window.jQuery = jQuery;
+/* eslint-enable no-multi-assign */
 
 class Site {
-    constructor() {
-        this.parseContent();
-    }
+  constructor() {
+    this.parseContent();
+  }
 
-    find(selector) {
-        return this.root.matches(selector) ? [this.root] : Array.from(this.root.querySelectorAll(selector));
-    }
+  find(selector) {
+    return this.root.matches(selector) ?
+      [this.root] :
+      Array.from(this.root.querySelectorAll(selector));
+  }
 
-    parseContent(root = document.body) {
-        this.root = root;
+  parseContent(root = document.body) {
+    this.root = root;
 
-        this.find('.page-wrapper').forEach((el) => {
-            console.log(el);
-        });
-    }
+    this.find('.page-wrapper').forEach((el) => {
+      /* eslint-disable no-console */
+      console.log(el);
+      /* eslint-enable no-console */
+    });
+  }
 }
 
 new Site();

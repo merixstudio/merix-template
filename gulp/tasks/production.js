7229113456450d474529a60396bfbdf92ca9d358
Slawomir Talarczyk <s.talarczyk@merixstudio.com>
2018-11-16 14:06:40 +0100

    Update to node 8, remove gulp callback

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -4,6 +4,6 @@ import {
 } from 'gulp';
 
 task('production', (done) => {
-    series('clean', 'icons', 'fonts', 'styles:production', 'polyfills:production', 'images:production', 'media:production', 'views:production', 'scripts:production', cb)();
+    series('clean', 'icons', 'fonts', 'styles:production', 'polyfills:production', 'images:production', 'media:production', 'views:production', 'scripts:production')();
     done();
 });
